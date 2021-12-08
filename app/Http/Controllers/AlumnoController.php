83cf7ce0bca74bc9de1af7ecd2d8678ac91d54d0
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2021-12-08 16:52:50 -0600

    Update AlumnoController.php

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Alumno;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreAlumnoRequest;
 use App\Http\Requests\UpdateAlumnoRequest;
 
@@ -13,74 +13,122 @@ class AlumnoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    //FUNCION PARA LEER
+    public function index(){
+        $alumno =Alumno::paginate(10);
+        return view ('Alumnos')->with('alumnos', $alumno);
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    //FUNCION PARA BUSCAR
+    public function show($id){
+        $alumno = Alumno::findorfail($id);
+        return view ('AlumnoIndividual')->with('alumnos', $alumno);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreAlumnoRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreAlumnoRequest $request)
-    {
-        //
+     //FUNCION PARA INSERTAR DATOS
+    public function crear(){
+        return view ('FormularioAlumno');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Alumno  $alumno
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Alumno $alumno)
-    {
-        //
+
+    //FUNCION PARA EDITAR LOS DATOS
+    public function edit($id){
+        $alumno = Alumno::findorfail($id);
+        return view ('FormularioEditarAlumno')->with('alumnos', $alumno);
+
+    } 
+
+
+
+            //FUNCION PARA CREAR LOS DATOS
+            public function store(Request $request){
+
+                //VALIDAR LOS CAMPOS
+               $request->validate([
+                'primer_nombre'=>'required | alpha',
+                'primer_apellido'=>'required | alpha',
+                'edad'=>'required | numeric',
+                'telefono'=>'required | numeric',
+                'direccion'=>'required',
+                'jornada'=>'required',
+                'grado_id'=>'required'
+               ]);
+
+                //SE CREA EL OBJETO
+               $nuevoAlumno = new Alumno();
+       
+                //FORMULARIO
+              $nuevoAlumno->primer_nombre=$request->input('primer_nombre');
+              $nuevoAlumno->primer_apellido=$request->input('primer_apellido');
+              $nuevoAlumno->edad=$request->input('edad');
+              $nuevoAlumno->telefono=$request->input('telefono');
+              $nuevoAlumno->direccion=$request->input('direccion');
+              $nuevoAlumno->jornada=$request->input('jornada');
+              $nuevoAlumno->grado_id=$request->input('grado_id');
+               
+           
+        
+                //PARA VERIFICAR SI SE CREO CORRECTAMENTE
+               $creado = $nuevoAlumno->save();
+       
+               if ($creado){
+                return redirect('/Alumnos/')->with('mensaje', 'El Alumno fue creado exitosamente');
+               // return redirect()->route('alumno.index')->with('mensaje', 'El Alumno fue creado exitosamente');
+               //} else {
+                   //Retornar con un mensaje de error
+               }
+           }
+
+
+
+
+    
+        //FUNCION PARA ACTUALIZAR LOS DATOS
+        public function update(Request $request, $id){
+
+            //VALIDAR LOS CAMPOS
+           $request->validate([
+               'primer_nombre'=>'required | alpha',
+               'primer_apellido'=>'required | alpha',
+               'edad'=>'required | numeric',
+               'telefono'=>'required | numeric',
+               'direccion'=>'required',
+               'jornada'=>'required',
+               'grado_id'=>'required'
+           ]);
+   
+           $alumno = Alumno::findorfail($id);
+   
+            //FORMULARIO
+           $alumno->primer_nombre=$request->input('primer_nombre');
+           $alumno->primer_apellido=$request->input('primer_apellido');
+           $alumno->edad=$request->input('edad');
+           $alumno->telefono=$request->input('telefono');
+           $alumno->direccion=$request->input('direccion');
+           $alumno->jornada=$request->input('jornada');
+           $alumno->grado_id=$request->input('grado_id');
+           
+       
+    
+            //PARA VERIFICAR SI SE CREO CORRECTAMENTE
+           $creado = $alumno->save();
+   
+           if ($creado){
+            return redirect('/Alumnos/')->with('mensaje', 'El Alumno fue modificado exitosamente');
+               //return redirect()->route('alumno.index')->with('mensaje', 'El Alumno fue modificado exitosamente');
+           //} else {
+               //Retornar con un mensaje de error
+           }
+       }
+
+    public function destroy($id){
+        Alumno::destroy($id);
+
+        //REDIRIGIR A LA RUTA ESTUDIANTE
+        return redirect('/Alumnos/')->with('mensaje', 'Alumno borrado completamente');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Alumno  $alumno
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Alumno $alumno)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateAlumnoRequest  $request
-     * @param  \App\Models\Alumno  $alumno
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateAlumnoRequest $request, Alumno $alumno)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Alumno  $alumno
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Alumno $alumno)
-    {
-        //
-    }
+    
 }
