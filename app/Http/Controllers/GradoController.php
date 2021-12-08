9c954c41d94520ce70138724d24a5d5b927f0609
LauraBustillo <93621801+LauraBustillo@users.noreply.github.com>
2021-12-08 16:54:01 -0600

    Update GradoController.php

--- app/Http/Controllers/GradoController.php
+++ app/Http/Controllers/GradoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profesor;
 use App\Models\Grado;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreGradoRequest;
 use App\Http\Requests\UpdateGradoRequest;
 
@@ -13,74 +15,115 @@ class GradoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    //FUNCION PARA LEER
+    public function index(){
+        $grado =Grado::paginate(10);
+        
+        return view ('Grados')->with('grados', $grado);
+
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    //FUNCION PARA BUSCAR
+    public function show($id){
+        $grado = Grado::findOrFail($id);
+        $Profesor = Grado::find($id);
+        $Profesor->profesors;
+        $Alumnos = Grado::find($id);
+        $Alumnos->alumnos;
+        return view ('GradoIndividual', compact('Profesor', 'Alumnos'))->with('grados', $grado);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreGradoRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreGradoRequest $request)
-    {
-        //
+     //FUNCION PARA INSERTAR DATOS
+    public function crear(){
+        return view ('FormularioGrado');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Grado  $grado
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Grado $grado)
-    {
-        //
+
+    //FUNCION PARA EDITAR LOS DATOS
+    public function edit($id){
+        $grado = Grado::findorfail($id);
+        return view ('FormularioEditarGrado')->with('grados', $grado);
+
+    } 
+
+
+
+            //FUNCION PARA CREAR LOS DATOS
+            public function store(Request $request){
+
+                //VALIDAR LOS CAMPOS
+               $request->validate([
+                'grado'=>'required | numeric',
+                'seccion'=>'required | alpha',
+                'aula'=>'required | numeric',
+                
+               ]);
+
+                //SE CREA EL OBJETO
+               $nuevoGrado = new Grado();
+       
+                //FORMULARIO
+              $nuevoGrado->grado=$request->input('grado');
+              $nuevoGrado->seccion=$request->input('seccion');
+              $nuevoGrado->aula=$request->input('aula');
+              
+               
+           
+        
+                //PARA VERIFICAR SI SE CREO CORRECTAMENTE
+               $creado = $nuevoGrado->save();
+       
+               if ($creado){
+                return redirect('/Grados/')->with('mensaje', 'El Grado fue creado exitosamente');
+               // return redirect()->route('alumno.index')->with('mensaje', 'El Alumno fue creado exitosamente');
+               //} else {
+                   //Retornar con un mensaje de error
+               }
+           }
+
+
+
+
+    
+        //FUNCION PARA ACTUALIZAR LOS DATOS
+        public function update(Request $request, $id){
+
+            //VALIDAR LOS CAMPOS
+           $request->validate([
+            'grado'=>'required | numeric',
+            'seccion'=>'required | alpha',
+            'aula'=>'required | numeric',
+              
+
+           ]);
+   
+           $grado = Grado::findorfail($id);
+   
+            //FORMULARIO
+            $grado->grado=$request->input('grado');
+            $grado->seccion=$request->input('seccion');
+            $grado->aula=$request->input('aula');
+           
+       
+    
+            //PARA VERIFICAR SI SE CREO CORRECTAMENTE
+           $creado = $grado->save();
+   
+           if ($creado){
+            return redirect('/Grados/')->with('mensaje', 'El Grado fue modificado exitosamente');
+               //return redirect()->route('alumno.index')->with('mensaje', 'El Alumno fue modificado exitosamente');
+           //} else {
+               //Retornar con un mensaje de error
+           }
+       }
+
+    public function destroy($id){
+        Grado::destroy($id);
+
+        //REDIRIGIR A LA RUTA ESTUDIANTE
+        return redirect('/Grados/')->with('mensaje', 'Grado borrado completamente');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Grado  $grado
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Grado $grado)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateGradoRequest  $request
-     * @param  \App\Models\Grado  $grado
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateGradoRequest $request, Grado $grado)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Grado  $grado
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Grado $grado)
-    {
-        //
-    }
 }
